b16fadd821c013d256b256056ae5d4c60713fec0
Gudmundur <gru007@post.uit.no>
2026-04-07 03:33:29 +0200

    Add PathHelper.cs util to reduce repeat code elsewhere (yet to be implemented in the code)

---
+++ GUI_G17_AITFMD/spectral-assist/SpectralAssist/Utils/PathHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace SpectralAssist.Utils;
+
+public class PathHelper
+{
+    public static string ResolveMacResourceFork(string filePath)
+    {
+        if (!Path.GetFileName(filePath).StartsWith("._")) return filePath;
+        var dir = Path.GetDirectoryName(filePath);
+        var actualName = Path.GetFileName(filePath)[2..];
+        var actualPath = !string.IsNullOrEmpty(dir) ? Path.Combine(dir, actualName) : actualName;
+        return File.Exists(actualPath) ? actualPath : filePath;
+    }
+}
